86855dff04d382fdb7c1259f78343c86e6eaf634
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2017-07-04 11:48:59 +0200

    Rename project to be consistent with package name

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from setuptools import setup, find_packages
 
 setup (
-	name='ast-vcard-callerid',
+	name='astvcardcallerid',
 	version="0.10",
 	description="Asterisk VCard Caller ID Lookup",
 	author='Dr. Torge Szczepanek',
 	author_email='debian@cygnusnetworks.de',
 	license='Apache License 2.0',
-	entry_points={'console_scripts': ['ast-vcard-callerid = astvcardcallerid.fastagi:main']},
+	entry_points={'console_scripts': ['astvcardcallerid = astvcardcallerid.fastagi:main']},
 	packages=['astvcardcallerid'],
 	install_requires=["configobj", "vobject", "phonenumbers"],
 )
